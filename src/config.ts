6f99192883281da5cbba3dadf59b54515d70a1a8
valiantone <zrjohn@yahoo.com>
2023-12-03 17:44:32 +0530

    feat: edits blog meta

--- src/config.ts
+++ src/config.ts
@@ -1,10 +1,10 @@
 import type { Site, SocialObjects } from "./types";
 
 export const SITE: Site = {
-  website: "https://astro-paper.pages.dev/", // replace this with your deployed domain
-  author: "Sat Naing",
-  desc: "A minimal, responsive and SEO-friendly Astro blog theme.",
-  title: "AstroPaper",
+  website: "https://valiantone.github.io", // replace this with your deployed domain
+  author: "Zubin J",
+  desc: "A blog featuring na variety of digital fundamentals ranging from career to code, data to design.",
+  title: "Frequent Bayesianism",
   ogImage: "astropaper-og.jpg",
   lightAndDarkMode: true,
   postPerPage: 3,
