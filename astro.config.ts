6780d37a2b83909fd586126962b3f6452492d7cd
valiantone <zrjohn@yahoo.com>
2023-12-03 16:43:24 +0530

    feat: deploys

--- astro.config.ts
+++ astro.config.ts
@@ -8,7 +8,7 @@ import { SITE } from "./src/config";
 
 // https://astro.build/config
 export default defineConfig({
-  site: SITE.website,
+  site: "https://valiantone.github.io",
   integrations: [
     tailwind({
       applyBaseStyles: false,
